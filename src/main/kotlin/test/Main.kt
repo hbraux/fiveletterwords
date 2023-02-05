a7a562118702c5c4d18ca49e03acca76eca2a633
Harold Braux <harold.braux@bleckwen.ai>
2023-02-05 14:44:42 +0100

    add to repo

--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -9,31 +9,31 @@ object Main {
   @JvmStatic fun main(args : Array<String>) {
     val words = File("words.txt").readLines().filterNot { it.length != it.toSet().size }.toMutableList()
     val elapsed = measureTimeMillis {
-      process(words)
+      process(words)?.let { println ("found ${it.chunked(5)}") }
     }
-    println(elapsed)
+    println("Elasped = ${elapsed/1000.0}")
   }
 
-  private fun process(words: MutableList<String>) {
+  private fun process(words: MutableList<String>): String? {
     LETTER_FREQUENCIES.forEach { letter ->
-      println("processing words with letter $letter")
       while (words.isNotEmpty()) {
         val word = pickWord(words, letter) ?: break
-        findNext(word, words)
+        findNext(word, words)?.let { return(it) }
         words.remove(word)
         println("Remaining: ${words.size}")
       }
     }
+    return null
   }
 
-  private fun findNext(current: String, words: List<String>) {
-    if (current.length >= 25) {
-      println("Found: $current !")
-      System.exit(0)
-    }
+  private fun findNext(current: String, words: List<String>): String? {
+    if (current.length >= 22)
+      return current
     val wordchars = current.toSet()
     val filtered = words.filterNot { w -> w.any { wordchars.contains(it) } }
-    filtered.forEach { findNext(it + current, filtered) }
+    return filtered.firstNotNullOfOrNull {
+      findNext(it + current, filtered)
+    }
   }
 
   private fun pickWord(words: List<String>, letter: Char): String? = words.firstOrNull { it.contains(letter) }
