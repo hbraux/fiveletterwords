1dd1bc40632024bde12a5ea0e7d36b14be04da0a
Harold Braux <harold.braux@bleckwen.ai>
2023-02-11 17:15:58 +0100

    rust v3, 3ms

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,29 +14,23 @@ fn main() {
         pwords.push(w.as_bytes());
     }
     let before = Instant::now();
-    let res = process("".as_bytes(), pwords);
-    println!("Result: {:?} in {:.2?} s", res, before.elapsed());
+    let result = process(&[], pwords);
+    println!("Result: {:?} in {} ms", result.unwrap(), before.elapsed().as_millis());
 }
 
-fn process(current: &[u8], words: Vec<&[u8]>) -> String {
+fn process(current: &[u8], words: Vec<&[u8]>) -> Option<String> {
     if current.len() >= 25 {
-        return String::from_utf8(Vec::from(current)).unwrap();
+        return Some(String::from_utf8(Vec::from(current)).unwrap());
     }
     let letter = LETTERS.into_iter().find(|b| !has_letters(current, b)).unwrap();
     let (first, second): (Vec<_>, Vec<_>) = words.into_iter().partition(|w| w.contains(letter));
     for word in first {
-        let mut filtered: Vec<&[u8]> = Vec::new();
-        for w in second.iter() {
-            if !share_letters(w, word) {
-                filtered.push(w)
-            }
-        }
-        let found = process([current, word].concat().deref(), filtered);
-        if !found.is_empty() {
-            return found;
+        if let Some(found) = process([current, word].concat().deref(),
+                                 second.clone().into_iter().filter(|w| !share_letters(w, word)).collect()) {
+            return Some(found)
         }
     }
-    return "".to_string();
+    return None
 }
 
 
