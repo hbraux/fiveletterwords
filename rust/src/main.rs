8d4335336106c34735a8ac39841a26b04adf3af9
Harold Braux <harold.braux@bleckwen.ai>
2023-02-11 15:12:28 +0100

    rust v0

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,43 +5,66 @@ use std::path::Path;
 static LETTERS: &[u8] = "etaonrishdlfcmugypwbvkjxzq".as_bytes();
 
 fn main() {
-    let words: Vec<String> = lines_from_file("../words.txt")
-        .expect("cannot load file")
-        .into_iter()
-        .filter(|w| has_no_duplicate(w))
-        .collect();
+    let words: Vec<String> = read_lines("../words.txt").expect("cannot read file")
+        .into_iter().filter(|w| !has_duplicate(w)).collect();
     println!("{} words", words.len());
     let res = process("", words);
     println!("Result: {}", res);
 }
 
 fn process(current: &str, words: Vec<String>) -> &str {
-    if current.len() >= 25 {
+    if current.len() >= 20 {
         return current;
     }
     let cbytes = current.as_bytes();
     let letter = LETTERS.into_iter().find(|b| !cbytes.contains(b)).unwrap();
-    let (first, second): (Vec<_>, Vec<_>) = words.into_iter().partition(|w| contain(w, letter));
+    let (first, second): (Vec<_>, Vec<_>) = words.into_iter().partition(|w| has_letter(w, letter));
     for word in first {
-        let f = second.into_iter().filter(|w| share_letters(&word, w)).collect();
+        let f = second.clone().into_iter().filter(|w| !share_letters(w, &word)).collect();
         process(&(word + &current), f);
     }
-    return ""
+    return "";
 }
 
-fn contain(word: &String, letter: &u8) -> bool {
-     word.as_bytes().contains(letter)
+fn has_letter(word: &str, letter: &u8) -> bool {
+    word.as_bytes().contains(letter)
 }
 
-fn share_letters(word: &String, other: &String) -> bool {
-    other.as_bytes().into_iter().any(|c| contain(word,c))
+fn share_letters(word: &str, current: &String) -> bool {
+    current.as_bytes().into_iter().any(|c| has_letter(word, c))
 }
 
-fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+fn read_lines(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn has_no_duplicate(word: &str) -> bool {
+fn has_duplicate(word: &str) -> bool {
     let bytes = word.as_bytes();
     (1..bytes.len()).any(|i| bytes[i..].contains(&bytes[i - 1]))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_duplicate() {
+        assert_eq!(false, has_duplicate("abcde"));
+        assert_eq!(true, has_duplicate("abccd"));
+        assert_eq!(true, has_duplicate("abcda"));
+    }
+
+    #[test]
+    fn test_has_letter() {
+        let letter= "z".as_bytes()[0];
+        assert_eq!(false, has_letter("abcde", &letter));
+        assert_eq!(true, has_letter("abzde", &letter));
+    }
+
+    #[test]
+    fn test_share_letters() {
+        let current = "abcdefghij".to_string();
+        assert_eq!(false, share_letters("uvwxy", &current));
+        assert_eq!(true, share_letters("uvaxy", &current));
+    }
 }
