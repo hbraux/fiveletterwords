62b3e7f75026ad953bdad319ec3466c51c878e24
Harold Braux <harold.braux@bleckwen.ai>
2023-02-12 15:47:02 +0100

    rust v3, 3ms

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,46 +1,42 @@
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::fs::read_to_string;
 use std::ops::Deref;
-use std::path::Path;
 use std::time::Instant;
 
 static LETTERS: &[u8] = "etaonrishdlfcmugypwbvkjxzq".as_bytes();
 
+// assuming ascii words
+type Word = [u8];
 
 fn main() {
-    let binding = read_lines("../words.txt").expect("cannot read file");
-    let pwords: Vec<&[u8]> = binding.iter().filter(|w| !has_duplicate(w)).map(|x| x.as_bytes()).collect();
     let before = Instant::now();
-    let result = process(&[], pwords);
-    println!("Result: {:?} in {} ms", result.unwrap(), before.elapsed().as_millis());
+    let str = read_to_string("../words.txt").expect("cannot read");
+    let words: Vec<&Word> = str.lines().filter(|w| !has_duplicate(w)).map(|x| x.as_bytes()).collect();
+    let result = process(&[], words);
+    println!("Result: {:?} found in {} ms", result.unwrap(), before.elapsed().as_millis());
 }
 
-fn process(current: &[u8], words: Vec<&[u8]>) -> Option<String> {
+fn process(current: &Word, words: Vec<&Word>) -> Option<String> {
     if current.len() >= 25 {
-        return Some(String::from_utf8(Vec::from(current)).unwrap())
+        return Some(String::from_utf8(Vec::from(current)).unwrap());
     }
     let letter = LETTERS.into_iter().find(|b| !has_letters(current, b)).unwrap();
     let (first, second): (Vec<_>, Vec<_>) = words.into_iter().partition(|w| w.contains(letter));
     for word in first {
-        // could be even faster without the clone
-        if let Some(x) = process([current, word].concat().deref(), second.clone().into_iter().filter(|w| !share_letters(w, word)).collect()) {
-            return Some(x)
+        if let Some(x) = process(
+            [current, word].concat().deref(),
+            second.clone().into_iter().filter(|w| !share_letters(w, word)).collect()) {
+            return Some(x);
         }
     }
-    return None
+    return None;
 }
 
-
-fn share_letters(word: &[u8], other: &[u8]) -> bool {
-    word.into_iter().any(|c| has_letters(other,c))
+fn share_letters(word: &Word, other: &Word) -> bool {
+    word.into_iter().any(|c| has_letters(other, c))
 }
 
-fn has_letters(word: &[u8], letter: &u8) -> bool  {
+fn has_letters(word: &Word, letter: &u8) -> bool {
     (1..word.len()).any(|i| word[i] == *letter)
-}
-
-fn read_lines(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-    BufReader::new(File::open(filename)?).lines().collect()
 }
 
 fn has_duplicate(word: &str) -> bool {
--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,29 +1,25 @@
 package test
 
 import java.io.File
-import kotlin.system.measureTimeMillis
 
 
 object Main {
 
   @JvmStatic fun main(args : Array<String>) {
+    val start = System.currentTimeMillis()
     val words = File("words.txt").readLines().filterNot { it.length != it.toSet().size }.toList()
-    val elapsed = measureTimeMillis {
-      process("", words)?.let { println ("Solution: ${it.chunked(5)}") }
-    }
-    println("Time: $elapsed ms")
+    val solution = process("", words)!!
+    println("Solution: ${solution.chunked(5)} found in ${System.currentTimeMillis() - start} ms")
   }
 
   private fun process(current: String, words: List<String>): String? {
     if (current.length >= 25) return current
-    val letter = LETTER_FREQUENCIES.first { !current.hasLetter(it) }
+    val letter = LETTER_FREQUENCIES.first { !current.contains(it) }
     val parts = words.partition { it.contains(letter) }
     return parts.first.firstNotNullOfOrNull {
-      word -> process(word + current, parts.second.filterNot { w -> w.any { word.hasLetter(it) } })
+      word -> process(word + current, parts.second.filterNot { w -> w.any { word.contains(it) } })
     }
   }
-
-  private fun String.hasLetter(letter: Char): Boolean = this.contains(letter)
 
   private const val LETTER_FREQUENCIES = "etaonrishdlfcmugypwbvkjxzq"
 }
